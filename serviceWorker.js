2bd37281ca74a3bb71bafdf7976df3269baab525
lixin <1121705040@qq.com>
2024-12-12 11:13:06 +0800

    修复离线刷新失效问题

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,20 +1,53 @@
 // 离线页面和资源
 
-self.addEventListener("fetch", (req)=>{
-    caches.match(req.request).then(async (response) => {
-    console.log('response',response);
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+    caches.match(event.request).then(async (response) => {
         if (!response) {
-        console.log('走查询');
-            let resp = await fetch(req.request);
+            console.log('走查询');
+            let resp = await fetch(event.request);
             let cache = await caches.open('pageV1');
-            cache.put(req.request, resp.clone())
+            cache.put(event.request, resp.clone())
             return resp;
         } else {
-        console.log('走缓存');
+            console.log('走缓存');
             return response;
         }
-    }).catch((e)=>{
-        console.log('请求失败!');
-        
     })
+    );
+
 })
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            "/",
+            "/index.html",
+            "css/main.css",
+            "js/main.js"
+        ]),
+    );
+});
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
+
+self.addEventListener('activate', event => {
+    console.log('激活方法');
+    event.waitUntil(clients.claim());
+
+    // event.waitUntil(
+    //     caches.keys().then(cacheNames => {
+    //         return Promise.all(
+    //             cacheNames.map(cacheName => {
+    //                 // if (cacheWhitelist.indexOf(cacheName) !== -1) {
+    //                     return caches.delete(cacheName);
+    //                 // }
+    //             })
+    //         );
+    //     })
+    // );
+    
+});
