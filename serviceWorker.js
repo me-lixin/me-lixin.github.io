340c20a4e58b5238e3cf4c6060027a373bc3393b
lixin <1121705040@qq.com>
2025-01-01 15:54:27 +0800

    启用了缓存方案，改成了将页面等静态资源存在IDB中，持久化的存储。

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,60 +1,105 @@
 // 监听请求
+const update = 'v3';
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-    caches.match(event.request).then(async (response) => {
-        if (!response) {
-            console.log('走查询',event.request);
-            return fetch(event.request)
-        } else {
-            console.log('走缓存');
-            return response;
-        }
-    })
-    );
+    let get = ()=>{
+        const crPage = pages.filter((page)=>event.request.url === page.url)
+        let myResponse = new Response(crPage[0].resp, {status:200,statusText:'OK'});
+        return myResponse;
+    }
+    event.respondWith(get());
 })
 //监听sw安装
-self.addEventListener("install", (event) => {
-    console.log('清理缓存');
+self.addEventListener("install", async (event) => {
     self.skipWaiting();
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
+        addResourcesToDB(
+            [
+                    "/",
+                    "/index.html",
+                    "/css/main.css",
+                    "/js/main.js",
+                    "/font/P3YMrUl62Myz.woff",
+                    "/font/P3YMrUl62Myz.woff2",
+                    "/images/add.svg",
+                    "/images/background.jpg",
+                    "/images/delete.svg",
+                    "/images/increase.svg",
+                    "/images/logo.svg",
+                    "/images/menu.svg",
+                    "/images/reduce.svg",
+                    "/images/reset.svg",
+                    "/images/return.svg",
+                    "/images/start.svg",
+                    "/images/stop.svg",
+                ]
+        )
     );
 
 });
-//将页面放进缓存
-const addResourcesToCache = async (resources) => {
-    const cache = await caches.open("v2");
-    await cache.addAll(resources);
-};
 //监听激活
 self.addEventListener('activate', event => {
-    console.log('添加缓存');
+    // console.log('添加缓存');
     clients.claim();
-    event.waitUntil(
-        addResourcesToCache([
-            "/",
-            "index.html",
-            "css/main.css",
-            "js/main.js",
-            "font/P3YMrUl62Myz.woff",
-            "font/P3YMrUl62Myz.woff2",
-            "images/add.svg",
-            "images/background.jpg",
-            "images/delete.svg",
-            "images/increase.svg",
-            "images/logo.svg",
-            "images/menu.svg",
-            "images/reduce.svg",
-            "images/reset.svg",
-            "images/return.svg",
-            "images/start.svg",
-            "images/stop.svg",
-        ]),
-    );
+    event.waitUntil(selectResources());
+
 });
+//把资源添加到IDB中
+const addResourcesToDB = (resources) => {
+    for (const url of resources) {
+        fetch(url).then(resp=>resp.blob())
+            .then(blob=>{
+                let store;
+                if (!store){
+                    store = createStore(dbMode[0],'resource')
+                }
+                const result =store.put({'url':'http://localhost:8000'+url,'resp':blob})
+            })
+    }
+};
+//从IDB中获取资源
+const selectResources = () => {
+    const store = createStore(dbMode[0],'resource')
+    const result = store.getAll();
+    result.addEventListener('success', (e) => {
+        pages = [...e.target.result]
+        console.log('静态资源获取成功', e.target.result)
+    })
+};
+
+
+// 数据库连接常量
+dbName = 'page';
+dbV = 2;
+dbMode = ['readwrite','readonly'];
+let db;
+let pages =[];
+init();
+
+async function init() {
+    db = await initalizeDB();
+}
+// 初始化
+function initalizeDB(){
+    return new Promise(resolve=>{
+
+    // 打开数据库
+    const request = indexedDB.open(this.dbName, this.dbV);
+    // 打开成功赋值给全局db
+    request.onsuccess = (e) => {
+        resolve(e.target.result)
+        console.log('SW数据库打开：')
+    }
+    // 打开错误
+    request.onerror = (e) => {
+        alert("请允许我的 web 应用使用 IndexedDB！");
+    }
+    // 首次构建
+    request.onupgradeneeded = (e) => {
+        e.target.result.deleteObjectStore('resource')
+        e.target.result.createObjectStore('resource', { keyPath: 'url'})
+    }
+    })
+}
+function createStore(mode, storeName) {
+    return db.transaction(storeName, mode).objectStore(storeName);
+}
